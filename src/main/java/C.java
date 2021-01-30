813cbedd301c0263ce66e97faf1ddc16953a80f1
wushilei <396941223@qq.com>
2021-01-30 17:39:08 +0800

    在dev的c上开发新功能

--- src/main/java/C.java
+++ src/main/java/C.java
@@ -2,4 +2,11 @@ public class C {
     public void test(){
         System.out.println("新增C");
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("继续开发新功能");
+
+        return super.equals(obj);
+    }
 }
